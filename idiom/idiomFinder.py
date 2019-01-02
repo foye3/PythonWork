6e9997aeb355d6b23cce3026de1b485d0a8ef7fb
Fan Zhang <Fan.Zhang@cerner.com>
2019-01-02 15:22:06 -0500

    Add idiomFinder

---
+++ idiom/idiomFinder.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+import pinyin
+
+PINYIN = False
+TONE = False
+
+def idiomFinder(first_character):
+  with open('idiom.json') as f:
+    data = json.load(f)
+  for idiom in data:
+    if TONE:
+      if idiom['pinyin'].split()[0] == pinyin.get(first_character):
+        print(idiom['word'])
+    elif PINYIN:
+      if pinyin.get(idiom['word'][0],format="strip") == pinyin.get(first_character,format="strip"):
+        print(idiom['word'])
+    else:
+      if idiom['word'][0] == first_character:
+        print(idiom['word'])
+  
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('first_character', help='find idioms by first character')
+  parser.add_argument("-p", '--pinyin', help="find idioms by first character's pinyin", action="store_true")
+  parser.add_argument("-t", '--tone', help="find idioms by first character's pinyin and tone", action="store_true")
+
+  args = parser.parse_args()
+  
+  if args.pinyin:
+    PINYIN = True
+  
+  if args.tone:
+    TONE = True
+    
+  if args.first_character:
+    idiomFinder(args.first_character)
